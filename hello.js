98769123776a4462d7a6d255a20913663562e32b
thiagotrr <37023689+thiagotrr@users.noreply.github.com>
2018-06-16 12:07:37 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,13 +4,30 @@ var app = express();
 var multer  = require('multer');
 var upload = multer();
 
+var mongo = require('mongodb');
+var mongoose = require('mongoose');  
+mongoose.connect('mongodb://localhost:27017/seguranca');  
+var Schema = mongoose.Schema;
+
+var usuarioSchema = new Schema({  
+ id: {type: String, required: true},  
+ usuario: String,  
+ senha: String  
+}, {collection: 'usuario'});  
+
+var Usuarios = mongoose.model('usuario', usuarioSchema);  
+
 app.set('trust proxy', 1);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
-}))
+}));
+
+
 
 app.get('/', function (req, res) {
   if(req.session.auth==true) {
@@ -31,13 +48,43 @@ app.get('/', function (req, res) {
 
 app.post('/login', upload.array(), function (req, res, next) {
   console.log(req.body);
+  
+  Usuarios.findOne({ "usuario": req.body.nome, "senha": req.body.senha }, 
+			function(err, obj){
+				if (err) { 
+					console.error('erro no findByOne');
+					return false;
+				}
+				if (obj.id = ""){
+					console.log('usuário não encontrado');
+					return true;
+				}
+				console.log('Sucesso!'+obj.id);
+				return true;
+			});
+  
   if (req.session.auth==true) {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html><head><meta charset="utf-8"></head><body><p>Você já está logado</p></body></html>');
     res.end();
-  } else if(req.body.nome=="trr" && req.body.senha=="teste") {
+  } else if(Usuarios.findOne({ "usuario": req.body.nome, "senha": req.body.senha }, 
+			function(err, obj){
+				if (err) { 
+					console.error('erro no findByOne');
+					return false;
+				}
+				if (obj.id = ""){
+					console.log('usuário não encontrado');
+					return true;
+				}
+				console.log('Sucesso!'+obj.id);
+				return true;
+			});) {
+	  
+		/* Código default */
 		req.session.auth= true;
     	res.end('welcome to the session demo. refresh!');
+		
   } else {
 		res.end('try again');
   }
