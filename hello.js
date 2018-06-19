f97a03c271bfda15e569c845efb49ce023a53d10
thiagotrr <37023689+thiagotrr@users.noreply.github.com>
2018-06-19 19:15:57 -0300

    findById

--- hello.js
+++ hello.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost:27017/seguranca');
 var Schema = mongoose.Schema;
 
 var usuarioSchema = new Schema({  
- id: String,  
+ id: Number,  
  usuario: String,  
  senha: String  
 });  
